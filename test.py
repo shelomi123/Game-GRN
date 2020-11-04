5d44b9a3926ea6aa83c5dfa8a5766befae16eb71
shelomi123 <shelomipriskila1995@gmail.com>
2020-11-04 22:14:23 +0530

    commited

--- test.py
+++ test.py
@@ -1,4 +1,23 @@
-g=5643
-print (g)
+import random
 
-g="jhjhg"
+r = random.randint(1,49)
+print(r)
+
+g=int(input("Enter a number between 1 and 49 : "))
+
+
+while True:
+    if g==r:
+        print("You won")
+        break
+    elif g>r:
+        print("Guess is high")
+        g=int(input("Enter a number between 1 and 49 : "))
+
+    elif g<r:
+        print("Guess is low")
+        g=int(input("Enter a number between 1 and 49 : "))
+
+    else:
+        print("invalid number")
+        g=int(input("Enter a number between 1 and 49 : "))
