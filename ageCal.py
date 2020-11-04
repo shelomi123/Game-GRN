6ad656f5451565141ffa79791365f1ff15ea7b13
shelomi123 <shelomipriskila1995@gmail.com>
2020-11-05 01:17:45 +0530

    done

--- ageCal.py
+++ ageCal.py
@@ -1,2 +1,13 @@
 from datetime import date
 
+byear=input("Enter your birth year ex: 1995")
+bmonth=input("Enter your birth month ex:05")
+bday=input("Enter your birth day ex:23")
+
+print('Birthday is', (bday+"/"+bmonth+"/"+byear))
+
+today= date.today()
+
+age=today.year - int(byear)
+
+print(age)
